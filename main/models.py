1740803834d1b4bcdc4fec91cbba731a933e4e6d
Ulanq <ulan121212@mail.ru>
2019-08-19 13:20:24 +0600

    DB

--- main/models.py
+++ main/models.py
@@ -24,8 +24,7 @@ class Department(models.Model):
 #Тип заявки
 class Type_Of_Request(models.Model):
     name = models.CharField(max_length=50) #Название типа заявки(например: "починка принтера")
-    responsible = models.OneToOneField(Employee) #Отвечающий за выполнение
-    priority = models.IntegerField() #Приоритет выполнения от 1 до 5
+    responsible = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True) #Отвечающий за выполнение
 
     def __str__(self):
         return 'Тип заявки: {}'.format(self.name)
@@ -34,17 +33,18 @@ class Type_Of_Request(models.Model):
 #Заявка
 class Request(models.Model):
     date = models.DateField(default=datetime.now()) #Дата заявки
+    type = models.OneToOneField(Type_Of_Request, on_delete=models.SET_NULL, null=True)  # Тип заявления
     applicant_is_notified = models.BooleanField(default=False) #Заявитель оповещён
     executor_is_notified = models.BooleanField(default=False)  #Исполнитель оповещён
     operator_is_notified = models.BooleanField(default=False)  #Оператор оповещён
-    author = models.OneToOneField(Employee) #Автор заявки
-    applicant = models.OneToOneField(Employee) #Заявитель(ФИО, подразделение, почта, телефон)
+    author = models.OneToOneField(Employee, on_delete=models.SET_NULL, related_name='requests_authors', null=True) #Автор заявки
+    applicant = models.OneToOneField(Employee, on_delete=models.SET_NULL, related_name='requests_applicant', null=True) #Заявитель(ФИО, подразделение, почта, телефон)
     building = models.CharField(max_length=20) #Здание
     room = models.CharField(max_length=5) #Кабинет
-    type = models.OneToOneField(Type_Of_Request) #Тип заявления
     sap = models.IntegerField() #Id оборудования
     description = models.CharField(max_length=255) #Описание проблемы
     file = models.FileField(blank=True) #Приложенный файл
+    priority = models.IntegerField() #Приоритет выполнения
 
     def __str__(self):
         return 'Заявление: {} {}.{}. - {}'.format(self.applicant.surname, self.applicant.name[:1], self.applicant.patronymic[:1], self.type.name)
--- main/views.py
+++ main/views.py
@@ -2,4 +2,12 @@ from django.http import Http404, HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 from django.template.loader import render_to_string
+from main.models import Request
 
+def main(request):
+    all_requests = Request.objects.all()
+    context = {
+        'all_requests': all_requests,
+    }
+
+    return render(request, 'main/main.html', context)
