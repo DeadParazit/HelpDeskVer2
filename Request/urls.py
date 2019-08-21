fcd8162aa342d28fc898b85814eb0ce7c2ae8b3c
Ulanq <ulan121212@mail.ru>
2019-08-21 10:02:00 +0600

    main-view

--- Request/urls.py
+++ Request/urls.py
@@ -3,8 +3,9 @@ from django.conf.urls import url
 from . import views
 
 # это нужно, чтобы django понимал какую именно страницу "showSpecificNews" использовать(при наличии страницы с таким же названием в другом application)
-app_name = 'main'
+app_name = 'request'
 
 urlpatterns = [
     path('', views.create, name='createRequest'),
+    path('<int:id>/', views.show, name='showRequest'),
 ]
--- Request/views.py
+++ Request/views.py
@@ -2,7 +2,7 @@ from django.http import Http404, HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 from django.template.loader import render_to_string
-from main.models import Type_Of_Request, Employee
+from main.models import Type_Of_Request, Employee, Request
 
 #Main page
 def create(request):
@@ -11,6 +11,18 @@ def create(request):
     context = {
         'all_employers': all_employers,
         'all_request_types': all_request_types,
+    }
+
+    return render(request, 'Request/create_document.html', context)
+
+def show(request,id):
+    all_request_types = Type_Of_Request.objects.all()
+    all_employers = Employee.objects.all()
+    request = get_object_or_404(Request, pk=id)
+    context = {
+        'all_employers': all_employers,
+        'all_request_types': all_request_types,
+        'request': request,
     }
 
     return render(request, 'Request/create_document.html', context)
